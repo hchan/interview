95ae6b510a0365dfc4283d78ffba39eb854637d9
hchan <hchan@apache.org>
2020-10-15 17:00:11 -0700

    more concise

--- src/com/henry/interview/ReorderLogs.java
+++ src/com/henry/interview/ReorderLogs.java
@@ -10,64 +10,36 @@ public class ReorderLogs {
 	public static void main(String[] args) {
 		String[] logs = new String[] { "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig",
 				"let3 art zero" };
-
-		System.out.println(Arrays.toString(logs));
-		System.out.println(Arrays.toString(reorderLogFiles(logs)));
+		ReorderLogs soln = new ReorderLogs();
+		System.out.println(Arrays.deepToString( soln.reorderLogFiles(logs)));
 		//		["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
 
 	}
 
 	// https://medium.com/@akshay_ravindran/day-40-reorder-data-log-files-40abfc9a98e0
 	
-	public static String[] reorderLogFiles(String[] logs) {
-	
-		String [] retval = new String [logs.length];
-		List<String> letterLogs = new ArrayList<String>();
-		List<String> numberLogs = new ArrayList();
-		for (String log : logs) {
-			if (isDigitLog(log)) {
-				numberLogs.add(log);
-			} else {
-				letterLogs.add(log);
-			}
-		}
-		Collections.sort(letterLogs, (o1, o2) -> {
-			String[] s1 = o1.split(" ");
-			String[] s2 = o2.split(" ");
-			int len1 = s1.length;
-			int len2 = s2.length;
-			for (int i = 1; i < Math.min(len1, len2); i++) {
-				if (!s1[i].equals(s2[i])) {
-					return s1[i].compareTo(s2[i]);
-				}
-			}
-			return s1[0].compareTo(s2[0]);
-		});
+	public String[] reorderLogFiles(String[] logs) {
+        
+        Arrays.sort(logs, (a,b)->{
+            int identA = a.indexOf(" ") + 1;
+            int identB = b.indexOf(" ") + 1;
 
-		int index = 0;
-		for (String log : letterLogs) {
-			retval[index] = log;
-			index++;
-		}
-		for (String log : numberLogs) {
-			retval[index] = log;
-			index++;
-		}
-		return retval;
-	}
-
-	private static boolean isDigitLog(String log) {
-		for (int i = 0; i < log.length(); i++) {
-			if (log.charAt(i) == ' ') {
-				if (log.charAt(i+1) >= '0' && log.charAt(i+1) <= '9') { 
-					// Character.isDigit(ch)
-					return true;
-				} else {
-					return false;
-				}
-			}
-		}
-		return false;
-	}
+            boolean isLetterA = Character.isLetter(a.charAt(identA));
+            boolean isLetterB = Character.isLetter(b.charAt(identB));
+            if(isLetterA && isLetterB){
+                int cmp = a.substring(identA).compareTo(b.substring(identB));
+                if(cmp != 0) return cmp;
+                
+                return a.compareTo(b);
+            }else if(isLetterA && !isLetterB){
+                return -1;
+            }else if(!isLetterA && isLetterB){
+                return 1;
+            }else return 0;
+            
+        });
+        
+        return logs;
+    }
 
 }
